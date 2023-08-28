d53f3cf0bf137003021ef13a294c069efc9d3309
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-08-28 12:00:46 -0300

    install prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [UserModule, AuthModule, ConfigModule.forRoot({isGlobal: true})],
@@ -15,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     provide: APP_GUARD,
     useClass: JwtAuthGuard,
   },
+  PrismaService
 ],
 })
 export class AppModule {}
