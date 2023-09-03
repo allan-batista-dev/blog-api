1f5ede47d7ac244a532f78bd5e2a56346ac0ccbb
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-09-03 08:29:50 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { UnauthorizedErrorInterceptor } from './common/errors/interceptors/unaut
 import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interception';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
